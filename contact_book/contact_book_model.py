eb30fcc8be547d0fb4fe96ed5401034501009964
Liam Edgeworth <ledgeworth32@gmail.com>
2021-09-09 20:05:13 +0100

    insert functionality in model.

--- contact_book/contact_book_model.py
+++ contact_book/contact_book_model.py
@@ -1,5 +1,6 @@
+
 from PyQt5.QtCore import Qt
-from PyQt5.QtSql import QSqlDatabase, QSqlQuery, QSqlTableModel
+from PyQt5.QtSql import QSqlDatabase, QSqlQuery, QSqlRecord, QSqlTableModel
 import logging
 import sys
 
@@ -13,14 +14,40 @@ class ContactModel():
 
 
     def create_model(self):
+        """Sets up a QSLTable model for the contacts table."""
         model = QSqlTableModel()
         model.setTable('contacts')
         model.setEditStrategy(QSqlTableModel.OnFieldChange)
         model.select() # Populate model with data from SetTable table.
-        self.headers = ["ID", "Gender", "Title", "First Name", "Last Name", "Email", "Phone Number"]
+        self.headers = ["ID", "Gender", "Title", "First Name", "Last Name", 
+        "Email", "Phone Number"]
         for count, header in enumerate(self.headers):
             model.setHeaderData(count, Qt.Horizontal, header)
+
         return model
+
+    def insert_record(self, new_record_dict):
+        insert_query = QSqlQuery()
+        insert_query.prepare("""
+        INSERT INTO contacts (gender, title, first_name, 
+        last_name, email, phone_number)
+        VALUES (:gender, :title, :first_name, :last_name, :email, :phone_number);""")
+        
+        insert_query.bindValue(":gender", new_record_dict["gender"])
+        insert_query.bindValue(":title", new_record_dict["title"])
+        insert_query.bindValue(":first_name", new_record_dict["first_name"])
+        insert_query.bindValue(":last_name", new_record_dict["last_name"])
+        insert_query.bindValue(":email", new_record_dict["email"])
+        insert_query.bindValue(":phone_number", new_record_dict["phone_number"])
+        
+        if insert_query.exec():
+            logging.info("insert query made.")
+            self.model.select() # reload the model with new values.
+            return True
+        logging.error("insert query failed.")
+        return False
+
+
 
 
 def create_table():
